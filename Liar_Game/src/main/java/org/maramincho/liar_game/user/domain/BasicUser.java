09081cdeb3c065d3d3f84caccc60dfdd5f3f2f4f
MaraMincho <seemaster300@naver.com>
2025-01-28 23:00:45 +0900

    feat: create User Controller repository 연결

--- Liar_Game/src/main/java/org/maramincho/liar_game/user/domain/BasicUser.java
+++ Liar_Game/src/main/java/org/maramincho/liar_game/user/domain/BasicUser.java
@@ -1,23 +1,22 @@
 package org.maramincho.liar_game.user.domain;
 
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jdk.jfr.Frequency;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.maramincho.liar_game.user.dto.CreateUser;
 import org.maramincho.liar_game.user.entity.BasicUserEntity;
 import org.maramincho.liar_game.user.entity.BasicUserRecordEntity;
+import org.maramincho.liar_game.utils.common.Tuple;
 
 import java.util.UUID;
 
 @AllArgsConstructor
 @Getter
-public class DefaultUser {
+public class BasicUser {
     private String nickName;
     private String email;
     private String password;
 
-    public BasicUserEntity toEntity() {
+    public Tuple<BasicUserEntity, BasicUserRecordEntity> toEntity() {
         BasicUserEntity basicUser = BasicUserEntity.builder()
                 .nickName(nickName)
                 .email(email)
@@ -31,10 +30,10 @@ public class DefaultUser {
         basicUser.setUserRecord(recordEntity);
         recordEntity.setUser(basicUser);
 
-        return basicUser;
+        return new Tuple<>(basicUser, recordEntity);
     }
 
-    public DefaultUser(CreateUser.request request) {
+    public BasicUser(CreateUser.request request) {
         this.password = request.password();
         this.email = request.email();
         this.nickName = request.nickName();
@@ -45,6 +44,6 @@ public class DefaultUser {
     }
 
     private void setRandomNickName() {
-        this.nickName = UUID.randomUUID().toString();
+        this.nickName = UUID.randomUUID().toString().substring(0, 15);
     }
 }
