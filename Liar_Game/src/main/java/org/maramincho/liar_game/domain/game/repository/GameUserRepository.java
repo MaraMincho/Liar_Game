13278041110f475a9e5f4eea04aa5af15637578d
MaraMincho <seemaster300@naver.com>
2025-01-28 16:46:18 +0900

    feat: 폴더링 변경2

--- Liar_Game/src/main/java/org/maramincho/liar_game/domain/game/repository/GameUserRepository.java
+++ Liar_Game/src/main/java/org/maramincho/liar_game/domain/game/repository/GameUserRepository.java
@@ -1,5 +1,6 @@
-package org.maramincho.liar_game.domain.game;
+package org.maramincho.liar_game.domain.game.repository;
 
+import org.maramincho.liar_game.domain.game.entity.GameUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameUserRepository extends JpaRepository<GameUserEntity, Long> {
--- Liar_Game/src/main/java/org/maramincho/liar_game/domain/user/repository/BasicUserRecordRepository.java
+++ Liar_Game/src/main/java/org/maramincho/liar_game/domain/user/repository/BasicUserRecordRepository.java
@@ -1,5 +1,6 @@
-package org.maramincho.liar_game.domain.user;
+package org.maramincho.liar_game.domain.user.repository;
 
+import org.maramincho.liar_game.domain.user.entity.BasicUserRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BasicUserRecordRepository extends JpaRepository<BasicUserRecordEntity, Long> {
