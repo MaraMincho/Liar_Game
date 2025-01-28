6d6a8ef4d510741a02fd7ef77db2224d75a56700
MaraMincho <seemaster300@naver.com>
2025-01-28 18:04:16 +0900

    feat: create User logic 작성

--- Liar_Game/src/main/java/org/maramincho/liar_game/user/dto/CreateUser.java
+++ Liar_Game/src/main/java/org/maramincho/liar_game/user/dto/CreateUser.java
@@ -1,12 +1,8 @@
 package org.maramincho.liar_game.user.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
 public final class CreateUser {
 
-    public record request(String email, String password) {
+    public record request(String email, String password, String nickName) {
 
     }
 
